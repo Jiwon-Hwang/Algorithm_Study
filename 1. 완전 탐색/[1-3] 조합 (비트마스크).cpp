0da9c0f43a905619e59b9606440001065ebae4a6
Jiwon-Hwang <54762704+Jiwon-Hwang@users.noreply.github.com>
2020-08-04 15:57:32 +0900

    combination, bit mask

--- 1. 완전 탐색/[1-3] 조합 (비트마스크).cpp
+++ 1. 완전 탐색/[1-3] 조합 (비트마스크).cpp
@@ -11,18 +11,18 @@ using namespace std;
 
 int main() {
 	vector<int> v;
-	
+
 	for (int i = 1; i <= 5; i++) {
-		v.push_back(i); 
+		v.push_back(i);
 	}
 	int arr[5] = { 0, 0, 0, 1, 1 };
-	
+
 	do {
 		for (int i = 0; i < 5; i++) {
 			if (arr[i] == 0) cout << v[i] << ' ';
 		}
 		cout << '\n';
-	} while (next_permutation(arr, arr+5)); 
+	} while (next_permutation(arr, arr + 5));
 
 
 	return 0;
