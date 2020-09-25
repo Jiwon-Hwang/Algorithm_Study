16950ce0fe8610972672a9a6e22a3b127fd38b9d
Jiwon-Hwang <54762704+Jiwon-Hwang@users.noreply.github.com>
2020-09-25 17:32:14 +0900

    3항 연산자, vector

---
+++ 0. ACM/[11497] 통나무 건너뛰기.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstring>
+#include <string>
+#include <math.h>
+#include <tuple>
+#include <typeinfo>  // operator typeid
+
+using namespace std;
+
+int T;
+int N;
+
+int main(void) {
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> N;
+        vector<int> L_v;
+        int l;
+        for (int j = 0; j < N; j++) {
+            cin >> l;
+            L_v.push_back(l);
+        }
+        
+        sort(L_v.begin(), L_v.end());
+
+        int tmp_max = 0;
+
+        for (int m = 0; m < N - 2; m += 2) { tmp_max = tmp_max > L_v[m + 2] - L_v[m] ? tmp_max : L_v[m + 2] - L_v[m]; }
+
+        tmp_max = tmp_max > L_v[N - 1] - L_v[N - 2] ? tmp_max : L_v[N - 1] - L_v[N - 2];      
+
+        for (int n = 1; n < N - 2; n += 2) { tmp_max = tmp_max > L_v[n + 2] - L_v[n] ? tmp_max : L_v[n + 2] - L_v[n]; }
+
+        tmp_max = tmp_max > L_v[1] - L_v[0] ? tmp_max : L_v[1] - L_v[0];
+
+        cout << tmp_max << endl;
+    }
+
+    return 0;
+}
