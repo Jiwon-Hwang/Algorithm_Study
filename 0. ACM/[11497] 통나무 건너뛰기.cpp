7af3c1911d2e109f1f6e1154163ed36a8c915257
Jiwon-Hwang <54762704+Jiwon-Hwang@users.noreply.github.com>
2020-09-25 18:08:03 +0900

    3항연산자 vs max(), sort(), vector

--- 0. ACM/[11497] 통나무 건너뛰기.cpp
+++ 0. ACM/[11497] 통나무 건너뛰기.cpp
@@ -26,18 +26,28 @@ int main(void) {
             cin >> l;
             L_v.push_back(l);
         }
-        
+
         sort(L_v.begin(), L_v.end());
 
         int tmp_max = 0;
+        for (int k = 2; k < N; k++) {
+            tmp_max = max(tmp_max, L_v[k] - L_v[k - 2]);
+        }
 
-        for (int m = 0; m < N - 2; m += 2) { tmp_max = max(tmp_max, L_v[m + 2] - L_v[m]); }
-
-        tmp_max = max(tmp_max, L_v[N - 1] - L_v[N - 2]);
-
-        for (int n = 1; n < N - 2; n += 2) { tmp_max = max(tmp_max, L_v[n + 2] - L_v[n]); }
-
-        cout << max(tmp_max, L_v[1] - L_v[0]) << endl;
+        cout << tmp_max << endl;
     }
     return 0;
 }
+
+
+/*
+int tmp_max = 0;
+
+for (int m = 0; m < N - 2; m += 2) { tmp_max = max(tmp_max, L_v[m + 2] - L_v[m]); }
+
+tmp_max = max(tmp_max, L_v[N - 1] - L_v[N - 2]);
+
+for (int n = 1; n < N - 2; n += 2) { tmp_max = max(tmp_max, L_v[n + 2] - L_v[n]); }
+
+cout << max(tmp_max, L_v[1] - L_v[0]) << endl;
+*/
